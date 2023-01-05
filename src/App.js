675a3f31a65783a890a9dbc81241cd1d1a43a014
FifthLord <nborysenko777@gmail.com>
2023-01-05 23:19:39 +0200

    work with trine proj

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import ClassCounter from "./components/ClassCounter";
-import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+// import ClassCounter from "./components/ClassCounter";
+// import Counter from "./components/Counter";
+import './styles/App.css'
 
 function App() {
 
    return (
       <div className="App">
-         <Counter />
-         <ClassCounter />
+         {/* <Counter />
+         <ClassCounter /> */}
+         <PostItem post={{ id: 1, title: "JS", body: 'Description' }} />
       </div>
    );
 }
