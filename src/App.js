372fe55c22841137e7e8d48baee3422c82f901f5
FifthLord <nborysenko777@gmail.com>
2023-01-06 18:08:10 +0200

    work with props logic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
-import PostItem from "./components/PostItem";
+import React, { useState } from "react";
+import PostList from "./components/PostList";
+// import PostItem from "./components/PostItem";
 // import ClassCounter from "./components/ClassCounter";
 // import Counter from "./components/Counter";
 import './styles/App.css'
 
 function App() {
+   const [posts, setPosts] = useState([
+      { id: 1, title: "JS", body: 'Description' },
+      { id: 2, title: "JS 2", body: 'Description' },
+      { id: 3, title: "JS 3", body: 'Description' },
+   ]);
 
    return (
       <div className="App">
-         {/* <Counter />
-         <ClassCounter /> */}
-         <PostItem post={{ id: 1, title: "JS", body: 'Description' }} />
+         <form>
+            <input type='text' placeholder="Назва посту" />
+            <input type='text' placeholder="Опис посту" />
+            <button>Створити пост</button>
+         </form>
+         <PostList posts={posts} title='Пости про JS' />
       </div>
    );
 }
