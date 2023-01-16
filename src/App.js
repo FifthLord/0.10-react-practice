ff8b427ff066e9cc80c6c0c2d760bf1af570ffd3
FifthLord <nborysenko777@gmail.com>
2023-01-16 11:53:53 +0200

    add PostFilter component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useMemo, useState } from "react";
+import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
-import MyInput from "./components/UI/input/MyInput";
-import MySelect from "./components/UI/select/MySelect";
 import './styles/App.css'
 
 function App() {
@@ -12,21 +11,20 @@ function App() {
       { id: 3, title: "JS 3", body: 'Description' },
    ]);
 
-   const [selectedSort, setSelectedSort] = useState('');
-   const [searchQuery, setSearchQuery] = useState('');
+   const [filter, setFilter] = useState({ sort: '', query: '' })
 
    //додавання в масив відсортованих постів при зміні поля сортування в формі, або зміні поста
    const sortedPosts = useMemo(() => {
-      if (selectedSort) {
-         return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+      if (filter.sort) {
+         return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
       }
       return posts;
-   }, [selectedSort, posts]);
+   }, [filter.sort, posts]);
 
    //фільтрація масиву відсортованих постів при зміні значення в інпуті або в відсортованих постах 
    const sortedAndSearchedPosts = useMemo(() => {
-      return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
-   }, [searchQuery, sortedPosts]);
+      return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+   }, [filter.query, sortedPosts]);
 
    const createPost = (newPost) => {
       //*розгортаємо старі пости й додаємо туди новий об'єкт з новим ІД
@@ -38,31 +36,14 @@ function App() {
       setPosts(posts.filter(p => p.id !== post.id));
    }
 
-   //значення з форми сортування
-   const sortPosts = (sort) => {
-      setSelectedSort(sort)
-   }
-
    return (
       <div className="App">
          <PostForm create={createPost} />
          <hr style={{ margin: '15px 0' }} />
-         <div>
-            <MyInput
-               value={searchQuery}
-               onChange={e => setSearchQuery(e.target.value)}
-               placeholder='Пошук...'
-            />
-            <MySelect
-               value={selectedSort}
-               onChange={sortPosts}
-               defaultValue="Сортування"
-               options={[
-                  { value: 'title', name: "По назві" },
-                  { value: 'body', name: "По опису" }
-               ]}
-            />
-         </div>
+         <PostFilter
+            filter={filter}
+            setFilter={setFilter}
+         />
          {sortedAndSearchedPosts.length !== 0
             ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Пости про JS' />
             : <h2 style={{ textAlign: 'center' }}>Пости не знайдені!</h2>
