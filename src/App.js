fc9adcc31e99557a893fc286848a3a9bdae81c40
FifthLord <nborysenko777@gmail.com>
2023-02-03 12:30:36 +0200

    fix navigate logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/UI/navbar/Navbar";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
 import './styles/App.css'
@@ -8,9 +9,11 @@ import './styles/App.css'
 function App() {
    return (
       <BrowserRouter>
+         <Navbar />
          <Routes>
             <Route path="/about" element={<About />} exact />
             <Route path="/posts" element={<Posts />} exact />
+            <Route path='*' element={<Navigate to='/posts' replace />} />
          </Routes>
       </BrowserRouter>
    );
