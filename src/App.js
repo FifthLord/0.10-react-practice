3dbacb2fa8f310a320cb5dfbfc58938eeb86ed6b
FifthLord <nborysenko777@gmail.com>
2023-02-04 10:35:08 +0200

    refactoring appRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
 import Navbar from "./components/UI/navbar/Navbar";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
 import './styles/App.css'
 
 
@@ -10,11 +9,7 @@ function App() {
    return (
       <BrowserRouter>
          <Navbar />
-         <Routes>
-            <Route path="/about" element={<About />} exact />
-            <Route path="/posts" element={<Posts />} exact />
-            <Route path='*' element={<Navigate to='/posts' replace />} />
-         </Routes>
+         <AppRouter />
       </BrowserRouter>
    );
 }
