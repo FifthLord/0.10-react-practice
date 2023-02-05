b7f4c111109a243541a249205e432f3c4d54750d
FifthLord <nborysenko777@gmail.com>
2023-02-05 12:17:42 +0200

    add useParams & details

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import PostService from '../API/PostService';
+import Loader from '../components/UI/loader/Loader';
+import { useFetching } from '../hooks/useFetching';
 
 const PostPage = () => {
+   const params = useParams();
+   const [post, setPost] = useState({});
+   const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+      const response = await PostService.getById(params.id)
+      setPost(response.data);
+   });
+
+   useEffect(() => {
+      fetchPostById(params.id)
+   }, [])
+
    return (
       <div>
-         <h2>Сторінка посту</h2>
+         <h2>Сторінка посту з ІД = {params.id}</h2>
+         {isLoading
+            ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}><Loader /></div>
+            : <div>{post.id}. {post.title}</div>
+         }
       </div>
    );
 };
