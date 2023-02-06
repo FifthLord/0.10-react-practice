a1040616f05289f49dbed3eab764e2b710afc164
FifthLord <nborysenko777@gmail.com>
2023-02-06 17:10:31 +0200

    add comments func in post

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -9,14 +9,21 @@ import { useFetching } from '../hooks/useFetching';
 const PostPage = () => {
    const params = useParams();
    const [post, setPost] = useState({});
-   const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+   const [comments, setComments] = useState([]);
+   const [fetchPostById, isLoading, error] = useFetching(async () => {
       const response = await PostService.getById(params.id)
       setPost(response.data);
+   });
+   const [fetchComments, isComLoading, comError] = useFetching(async () => {
+      const response = await PostService.getCommentsById(params.id)
+      setComments(response.data);
    });
 
    useEffect(() => {
       fetchPostById(params.id)
+      fetchComments(params.id)
    }, [])
+
 
    return (
       <div>
@@ -25,6 +32,20 @@ const PostPage = () => {
             ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}><Loader /></div>
             : <div>{post.id}. {post.title}</div>
          }
+         <h2>Коментарі</h2>
+         {isComLoading
+            ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}><Loader /></div>
+            : <div>
+               {comments.map(comm =>
+                  <div style={{ marginTop: 15 }}>
+                     <h5>{comm.email}</h5>
+                     <div>{comm.body}</div>
+                  </div>
+               )}
+            </div>
+         }
+         <div>
+         </div>
       </div>
    );
 };
